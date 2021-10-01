a8edff33028847a7afda6ae4846580f00ca9b0ec
mrnmchnk <91681586+mrnmchnk@users.noreply.github.com>
2021-10-02 02:06:05 +0500

    Add files via upload

---
+++ TestProject/calc.php
@@ -0,0 +1,19 @@
+<?
+    // echo'111111';
+
+    // $t = $_POST['id'];
+    // $tmp = ['first'=>1, 'second' =>2];
+    // echo json_encode($tmp);
+    echo json_encode($_POST['period']);
+
+
+    // if(isset( $_GET["json"])) {
+    //     echo "111";
+    // }
+    // else {
+    //     echo "222";
+    // }
+    
+    // echo json_encode(var_dump($_POST));
+
+?>
---
+++ TestProject/script.js
@@ -0,0 +1,75 @@
+$( document ).ready(function() {
+    // console.log( "ready!" );
+    $(".button-submit").on('click',function(){
+        // let date = $(".date").val();
+        // let period = $(".period").val();
+        // let depositAmount = $(".depositAmount").val();
+        // let percent = $(".percent").val();
+        // let refillAmount = $(".refillAmount").val();
+        let request = {
+            date: $(".date").val(),
+            period: $(".period").val(),
+            depositAmount: $(".depositAmount").val(),
+            percent: $(".percent").val(),
+            refillAmount: $(".refillAmount").val(),
+        }
+        // let json = JSON.stringify(request);
+
+        // $.ajax({
+        //     url: 'calc.php',
+        //     success: function(data) {
+                // var txt = JSON.parse(txt);
+                
+        //     //   $('.result').text(data);
+
+        //     alert(JQuery.type(txt));
+
+        //     }
+        //   });
+
+        $.ajax({
+            url: './calc.php', /* Куда пойдет запрос */
+            method: 'post', /* Метод передачи (post или get) */
+            dataType: 'json', /* Тип данных в ответе (xml, json, script, html). */
+            // data: json, /* Параметры передаваемые в запросе. */
+            // data: 'name=Andrew&nickname=Aramis',
+            data: request,
+            
+
+            success: function(result){ /* функция которая будет выполнена после успешного запроса. */
+                // $('.result').text(data);
+                // var txt = JSON.parse(JSON.stringify(result));
+                // console.log(txt["first"]);
+                console.log(result);
+                // alert(data); /* В переменной data содержится ответ от index.php. */
+            }
+        });
+
+
+
+          
+
+        // alert(json);
+    });
+
+    $(".is-refillable").on('click', function() {
+        
+
+        if ($(this).is(':checked')){
+            $('.refillAmount').css("visibility", "visible");
+        }
+        else {
+            $('.refillAmount').css("visibility", "hidden");
+
+        }
+
+    });
+
+
+   
+
+
+});
+
+
+
