045bbe576052d77711fd921fc0e90a709bc3f07f
Nemchenko Max <mrnemchenko7@gmail.com>
2021-10-08 19:56:24 +0500

    calculator: Поправил код. Директория css

--- TestProject/calc.php
+++ TestProject/calc.php
@@ -1,76 +1,98 @@
 <?
-    // echo'111111';
-
-    // $t = $_POST['id'];
-    // $tmp = ['first'=>1, 'second' =>2];
-    // echo json_encode($tmp);
-    // echo json_encode($_POST['period']);
-    $period = intval($_POST["period"]);
-    $date = $_POST["date"]);
-    $refillAmount = intval($_POST["refillAmount"]);
-    $depositAmount = intval($_POST["depositAmount"]);
-    $percent = intval($_POST["percent"]);
-    $timetype = intval($_POST[".timetype"]);
-
-    $daysNboom = explode(".", $date);
-    $currentMonth = intval($daysNboom[1]);
-    $currentYear = intval($daysNboom[2]);
-    // echo $daysNboom[0];
-    // echo $daysNboom[1];
-    // $daysN = cal_days_in_month(CAL_GREGORIAN, $daysNboom[1], $daysNboom[0]);
-    // echo $daysN;
-    $daysY = (!(intval($currentYear) % 4)) ? 366 : 365;
-    // echo $daysY;
-    // echo gettype(intval($daysNboom[0]));
-    $sumN = 0;
-    $sum = 0;
-
-    for ($i = 0; $i < $period; $i++) {
-        if ($currentMonth > 12) {
-            $currentMonth = 1;
-            $currentYear++;
-            $daysY = (!(intval($currentYear) % 4)) ? 366 : 365;
-        }
-        $sum = $depositAmount + ($depositAmount + $refillAmount) * (cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear)) * ($percent / $daysY);
-        $currentMonth++;
-        // echo "Месяц " . ($i+1) . " = " . $sum;
-        // echo "<br>";
-        $depositAmount = $sum;
-        $sumN = intval($sum);
-    };
-    // echo $sumN;
-    echo json_encode($sumN);
-
-    // $daysN++;
-    // if ($daysN > 12) {
-    //     $daysN = 1;
-    // }
-    //daysNn =  cal_days_in_month(CAL_GREGORIAN, $daysNboom[1]+1, $daysNboom[0])
-        
-    // echo $depositAmount;
-    // echo "<br>";
-    // echo $refillAmount;
-    // echo "<br>";
-    // echo $daysN;
-    // echo "<br>";
-    // echo $period;
-    // echo "<br>";
-    // echo $daysY;
-   
 
 
-    // $number = cal_days_in_month(CAL_GREGORIAN, 8, 2003); // 31
-    // echo "Всего {$number} дней в Августе 2003 года"; 
+$json = file_get_contents('php://input'); 
+$obj = json_decode($json);
+// echo($obj->date);
 
 
 
-    // if(isset( $_GET["json"])) {
-    //     echo "111";
-    // }
-    // else {
-    //     echo "222";
-    // }
-    
-    // echo json_encode(var_dump($_POST));
+// $receiveData = $_POST['convertFormValueToJson'];
+
+// $respond = json_decode($receiveData);
+// print_r($receiveData);
+// echo ($receiveData);
+
+
+
+$date = ($obj->date);
+$period = intval($obj->period);
+$refillAmount = intval($obj->refillAmount);
+$depositAmount = intval($obj->depositAmount);
+$percent = intval($obj->percent);
+// $timetype = intval($obj->timetype);
+
+// echo $period;
+// echo "<br>";
+// echo $date;
+// echo "<br>";
+// echo $refillAmount;
+// echo "<br>";
+// echo $depositAmount;
+// echo "<br>";
+// echo $percent;
+
+
+
+// $period = intval($_POST["period"]);
+// $date = $_POST["date"];
+// $refillAmount = intval($_POST["refillAmount"]);
+// $depositAmount = intval($_POST["depositAmount"]);
+// $percent = intval($_POST["percent"]);
+// $timetype = intval($_POST[".timetype"]);
+
+$daysNboom = explode(".", $date);
+$currentMonth = intval($daysNboom[1]);
+$currentYear = intval($daysNboom[2]);
+// echo $daysNboom[0];
+// echo $daysNboom[1];
+// $daysN = cal_days_in_month(CAL_GREGORIAN, $daysNboom[1], $daysNboom[0]);
+// echo $daysN;
+$daysY = (!(intval($currentYear) % 4)) ? 366 : 365;
+// echo $daysY;
+// echo gettype(intval($daysNboom[0]));
+$sumN = 0;
+$sum = 0;
+
+for ($i = 0; $i < $period; $i++) {
+if ($currentMonth > 12) {
+$currentMonth = 1;
+$currentYear++;
+$daysY = (!(intval($currentYear) % 4)) ? 366 : 365;
+}
+$sum = $depositAmount + ($depositAmount + $refillAmount) * (cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear)) * ($percent / $daysY);
+$currentMonth++;
+
+$depositAmount = $sum;
+$sumN = intval($sum);
+// echo "Месяц " . ($i+1) . " = " . $sumN;
+// echo "<br>";
+};
+echo $sumN;
+// echo json_encode($sumN);
+
+// $daysN++;
+// if ($daysN > 12) {
+// $daysN = 1;
+// }
+//daysNn = cal_days_in_month(CAL_GREGORIAN, $daysNboom[1]+1, $daysNboom[0])
+
+
+
+
+
+// $number = cal_days_in_month(CAL_GREGORIAN, 8, 2003); // 31
+// echo "Всего {$number} дней в Августе 2003 года";
+
+
+
+// if(isset( $_GET["json"])) {
+// echo "111";
+// }
+// else {
+// echo "222";
+// }
+
+// echo json_encode(var_dump($_POST));
 
 ?>
--- TestProject/script.js
+++ TestProject/script.js
@@ -1,115 +1,78 @@
-$( document ).ready(function() {
-    // console.log( "ready!" );
-    $(".button-submit").on('click',function(){
-        // let date = $(".date").val();
-        // let period = $(".period").val();
-        // let depositAmount = $(".depositAmount").val();
-        // let percent = $(".percent").val();
-        // let refillAmount = $(".refillAmount").val();
-        let checker = 0;
-        if ($('#checkbox_id').is(":checked")) {
-            checker = $(".refillAmount").val();
-        } 
+$(document).ready(function () {
+    $(".button-submit").on("click", function (e) {
+        e.preventDefault();
+    let checker = 0;
+    if ($("#checkbox_id").is(":checked")) {
+        checker = $(".refillAmount").val();
+    }
+    
+    const getValueFromNameField = (nameField) => $(`.${nameField}`).val();
+    
+    const formValue = {
+    date: getValueFromNameField("datepicker-here"),
+    period:
+    getValueFromNameField("period") * getValueFromNameField("timetype"),
+    depositAmount: getValueFromNameField("depositAmount"),
+    percent: getValueFromNameField("percent"),
+    refillAmount: checker,
+    };
+    
+    const convertFormValueToJson = JSON.stringify(formValue);
+    console.log("convertFormValueToJson: ", convertFormValueToJson);
+    
+    $.ajax({
+        url: "./calc.php" ,
+        method: "post",
+        dataType: "json",
+        data: convertFormValueToJson,
+        success: (data) => {
+            $('.line').css({display: "block"});
+        $(".result").text(`Результат: ${data}`)},
+        error: (data) => $(".result").text(`При запросе произошла ошибка : ${data}`),
+    });
+    });
+    
+    $(".is-refillable").on("click", function () {
+    if ($(this).is(":checked")) {
+    $(".refillAmount").css("visibility", "visible");
+    } else {
+    $(".refillAmount").css("visibility", "hidden");
+    }
+    });
+    
+  
+    });
 
-        // alert(checker);
+    // $('.period').change(function(e) {
+    //     console.log('e: ', e.currentTarget.value);
+    // })
 
-        let request = {
-            date: $(".datepicker-here").val(),
-            period: ( $(".period").val() * ($('.timetype').val()) ),
-            depositAmount: $(".depositAmount").val(),
-            percent: $(".percent").val(),
-            refillAmount: checker,
+    // const blockCharsInput = (e, regexp) => {
+    //     const cursorPosition = e.target.selectionStart;
+    //     const prevValue = e.target.value;
+    //     const newValue = prevValue.slice(0, cursorPosition) +e.key + prevValue.slice(cursorPosition);
+    //     if(!newValue.match(regexp)) {
+    //         e.preventDefault();
+    //     }
+    // }
+
+
+    $(".calculator-form").validate({
+        submitHandler: function (formValue) {
+            
         }
-
-        // alert(request.period);
-        // let json = JSON.stringify(request);
-
-        // $.ajax({
-        //     url: 'calc.php',
-        //     success: function(data) {
-                // var txt = JSON.parse(txt);
-                
-        //     //   $('.result').text(data);
-
-        //     alert(JQuery.type(txt));
-
-        //     }
-        //   });
-
-        $.ajax({
-            url: './calc.php', /* Куда пойдет запрос */
-            method: 'post', /* Метод передачи (post или get) */
-            dataType: 'json', /* Тип данных в ответе (xml, json, script, html). */
-            // data: json, /* Параметры передаваемые в запросе. */
-            // data: 'name=Andrew&nickname=Aramis',
-            data: request,
-            
-            success: function(data){ /* функция которая будет выполнена после успешного запроса. */
-                // $('.result').text(data);
-                // console.log(txt["first"]);
-                let $boom = jQuery.parseJSON(data);
-
-                // alert(data); /* В переменной data содержится ответ от index.php. */
-                // JSON.parse($sumN),
-                // $getJSON($sumN); не то
-                alert(jQuery.type($boom));
-            },
-            
+    });
+    if ($calcForm) {
+        $calcForm.validate({
+        rules: {
+        date: required,
+        period: {
+        maxlength: 60,
+        },
+        },
+        messages: {
+        date: "ВВЕДИ",
+        period: "Не более пяти лет",
+        },
         });
-        
-    });
-
-
-    
-
-    $(".is-refillable").on('click', function() {
-
-        if ($(this).is(':checked')){
-            $('.refillAmount').css("visibility", "visible");
-        }
-        else {
-            $('.refillAmount').css("visibility", "hidden");
-        }
-
-    });
-
-
-    
-    $('.calculator-form').validate();
-        var $calcForm = $(".calculator-form");
-        if ($calcForm) {
-            $calcForm.validate({
-                rules: {
-                    date: required,
-                    period: {
-                        maxlength: 60
-                    },
-                    
-                },
-                messages: {
-                    date: "ВВЕДИ",
-                    period: "Не более пяти лет",
-                },
-            });
-        };
-
-
-
-        
-
-
-
-
-//     $timetype: $(".timetype").val(),
-//    $timetype.on('click', function() {
-//     ($('.timetype').val())
-//     });
-    // $(".btn-container").on('click', function() {
-    //     alert($('.timetype').val());
-    // });
-
-    
-
-});
-
-
+    }
